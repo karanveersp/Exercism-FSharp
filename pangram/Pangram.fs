d2cd07a50c8db82d38956c60060fadd03a0ce5be
karanveer <karan_4496@hotmail.com>
2021-04-04 10:43:40 -0400

    Pangram Updated Solution

--- pangram/Pangram.fs
+++ pangram/Pangram.fs
@@ -1,13 +1,7 @@
 module Pangram
 
 let isPangram (input: string): bool =
-
-    let containsChar (c: char): bool =
-        input
-        |> Seq.toArray
-        |> Seq.map System.Char.ToLower
-        |> Seq.contains (System.Char.ToLower(c))
-
-    "abcdefghijklmnopqrstuvwxyz"
-    |> Seq.toArray
-    |> Seq.forall containsChar
+    input.ToLower()
+    |> Seq.filter System.Char.IsLetter
+    |> Seq.distinct
+    |> Seq.length = 26
